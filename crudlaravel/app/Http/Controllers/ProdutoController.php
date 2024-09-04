9f93e943c97854174bfb01b3b933f5670f7a8909
nilknarf762 <frankbarros17@hotmail.com>
2024-09-04 08:06:04 -0300

    Composer autoload setup

--- crudlaravel/app/Http/Controllers/ProdutoController.php
+++ crudlaravel/app/Http/Controllers/ProdutoController.php
@@ -1,113 +1,180 @@
 <?php
 
+
 namespace App\Http\Controllers;
 
+
 use App\Models\Produto;
+
 use Illuminate\Http\Request;
+
 use Illuminate\Http\RedirectResponse;
+
 use Illuminate\Http\Response;
+
 use Illuminate\View\View;
 
+use Symfony\Contracts\Service\Attribute\Required;
+
+
 class ProdutoController extends Controller
+
 {
+
     /**
+
      * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
+
      */
+
     public function index(): View
+
     {
+
         //
-        $produto = Produto:: latest()->paginate(5);
 
-        return view('produtos.index',compact('produto'))
-         ->with('i',(request()->input('page',1)-1)*5);
+        $produtos = Produto::latest()->paginate(5);
+
+
+        return view('produtos.index',compact('produtos'))
+
+            ->with('i',(request()->input('page',1)-1)*5);
+
+
     }
 
+
     /**
+
      * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
+
      */
+
     public function create(): View
+
     {
+
         return view('produtos.create');
+
     }
 
+
     /**
+
      * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+
      */
+
     public function store(Request $request): RedirectResponse
-    {
-        $request-> validate([
-            'descricao' => 'required',
-            'qtd' => 'required',
-            'precoUnitario' => 'required',
-        ]);
-        Produto:: create($request->all());
 
-        return redirect()->route('produtos.index')
-                         ->with('success','Produto criado com sucesso.');
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Produto  $produto
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Produto $produto): View
     {
-        return view('produtos.show', compact(produto));
-    }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Produto  $produto
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Produto $produto): View
-    {
-        return view('produtos.edit', compact('produto'));
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Produto  $produtos
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Produto $produto): RedirectResponse
-    {
         $request->validate([
-           'descricao' => 'required',
-           'qtd' => 'required',
-           'precoUnitario' => 'required',
-           'precoVenda'=> 'required',
+
+            'descricao' => 'required',
+
+            'qtd' => 'required',
+
+            'precoUnitario' => 'required',
+
+            'precoVenda' => 'required',
+
         ]);
-        
-        $produto ->update($request->all());
+
+
+        Produto::create($request->all());
+
 
         return redirect()->route('produtos.index')
-                         -with('success', 'Produto atualizado com sucesso');
+
+                        ->with('success','Produto criado com sucesso.');
+
+
     }
 
+
     /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Produto  $produtos
-     * @return \Illuminate\Http\Response
+
+     * Display the specified resource.
+
      */
-    public function destroy(Produto $produto): RediretResponse
+
+    public function show(Produto $produto): View
+
     {
+
+        return view('produtos.show',compact('produto'));
+
+    }
+
+
+    /**
+
+     * Show the form for editing the specified resource.
+
+     */
+
+    public function edit(Produto $produto): View
+
+    {
+
+        return view('produtos.edit',compact('produto'));
+
+    }
+
+
+    /**
+
+     * Update the specified resource in storage.
+
+     */
+
+    public function update(Request $request, Produto $produto): RedirectResponse
+
+    {
+
+        $request->validate([
+
+            'descricao' => 'required',
+
+            'qtd' => 'required',
+
+            'precoUnitario' => 'required',
+
+            'precoVenda' => 'required',
+
+        ]);
+
+
+        $produto->update($request->all());
+
+
+        return redirect()->route('produtos.index')
+
+                        ->with('success','Produto atualizado com sucesso.');
+
+
+    }
+
+
+    /**
+
+     * Remove the specified resource from storage.
+
+     */
+
+    public function destroy(Produto $produto): RedirectResponse
+
+    {
+
         $produto->delete();
 
+
         return redirect()->route('produtos.index')
-                         ->widt('success','Produto excluido com sucesso');
+
+                        ->with('success','Produto excluído com sucesso.');
+
+
     }
+
 }
